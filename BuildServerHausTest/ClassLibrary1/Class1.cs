3c3e7f80a8da7f5c0f995bc1beb2b6f7188eee6c
ahmed1234 <alifahed9559@gmail.com>
2017-02-25 09:24:27 +0100

    use NuGet Packages

--- BuildServerHausTest/ClassLibrary1/Class1.cs
+++ BuildServerHausTest/ClassLibrary1/Class1.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using NUnit.Framework;
 namespace ClassLibrary1
 {
+    [TestFixture]
     public class Class1
     {
     }
